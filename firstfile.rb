02c39f390397d66cc4a81542065c9e0a2d7a163f
udomk <kufrekus4@gmail.com>
2022-08-14 01:00:05 +0100

    learnruby

--- firstfile.rb
+++ firstfile.rb
@@ -1,13 +1,16 @@
-# print "Hello World"
-# print " and fellows"
-#
-# puts "Hello World"
-# puts "and fellows"
-#
-# p "Hello World"
-# p "and fellows"
 
-# puts ("Unary")
-# puts (+30 + -6)
+=begin
+print "Hello World"
+print " and fellows"
+
+puts "Hello World"
+puts "and fellows"
+
+p "Hello World"
+p "and fellows"
+
+puts ("Unary")
+puts (+30 + -6)
+=end
 
 puts (14+4-9/2+9*2+1-12)
